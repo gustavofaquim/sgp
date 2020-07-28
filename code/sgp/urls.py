ab58bcf5a8daa0fcfbac87eef1e8dd48ea41f4de
Gustavo Faquim <gustavofaquim408@gmail.com>
2020-07-28 16:37:56 -0300

    Atualização no modelo de prova.

--- code/sgp/urls.py
+++ code/sgp/urls.py
@@ -14,12 +14,12 @@ urlpatterns = [
     path('lista_disciplina', views.lista_disciplina, name='lista_disciplina'),
     #Crud professor
     path('cadastro-professor/', views.cadastro_professor),
-    path('lista_professores/', views.lista_professor),
+    path('lista_professores/', views.lista_professor, name='lista_professores'),
     path('atualizar_professor/<int:cpf>/', views.atualizar_prof, name='atualizar_professor'),
     path('deletar_professor/<int:cpf>', views.deletar_prof, name='deletar_professor'),
     #Crud alternativa
     path('cadastro-alternativa/', views.cadastro_alternativa),
-    path('lista_alternativas/', views.lista_alternativa),
+    path('lista_alternativas/', views.lista_alternativa, name='lista_alternativas'),
     path('atualizar_alternativa/<int:id>/', views.atualizar_alter, name='atualizar_alternativa'),
     path('deletar_alternativa/<int:id>/', views.deletar_alter, name='deletar_alternativa'),
     #Crud questao
@@ -29,7 +29,7 @@ urlpatterns = [
     path('deletar_questao/<int:id>', views.deletar_quest, name='deletar_questao'),
     #Crud prova
     path('cadastro-prova/', views.cadastro_prova),
-    path('lista_prova/', views.lista_prova),
+    path('lista_prova/', views.lista_prova, name='lista_prova'),
     path('atualizar_prova/<int:id>', views.atualizar_prov, name='atualizar_prova'),
     path('deletar_questao/<int:id>', views.deletar_prov, name='deletar_prova'),
     path('gerar_prova/<int:id>', views.gerar_prova, name="gerar_prova"),
